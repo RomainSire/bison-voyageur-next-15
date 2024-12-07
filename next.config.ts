7dee1f342dc38628dcee05b69d77c55feabde0da
Romain <rsire@tuta.io>
2024-12-07 23:33:45 +0900

    config: add Directus SDK dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	images: {
+		domains: ["directus.romainsire.com"],
+	},
 };
 
 export default nextConfig;
