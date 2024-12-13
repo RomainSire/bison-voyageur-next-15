415e74d06e2aa72884adf01f1c92ad867e10b0ee
Romain <rsire@tuta.io>
2024-12-13 23:07:48 +0900

    feat: enhance Header component with animations and responsive styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Link from "next/link";
 import style from "./Header.module.css";
 
@@ -5,19 +7,39 @@ type HeaderProps = Readonly<{
 	className?: string;
 }>;
 
+/**
+ * Animated link component (with Motion)
+ */
+const MotionLink = motion(Link);
+
+/**
+ * Header component
+ */
 export default function Header({ className }: HeaderProps) {
 	return (
 		<nav className={`${className} ${style.nav}`}>
 			<ul className={style.navList}>
 				<li>
-					<Link href="/" className={style.links}>
+					<MotionLink
+						href="/"
+						className={style.links}
+						whileHover={{ scale: 1.1, y: -5 }}
+						whileTap={{ scale: 1 }}
+						transition={{ type: "spring", stiffness: 500 }}
+					>
 						Bison Voyageur
-					</Link>
+					</MotionLink>
 				</li>
 				<li>
-					<Link href="/menu" className={style.links}>
+					<MotionLink
+						href="/menu"
+						className={style.links}
+						whileHover={{ scale: 1.1, y: -5 }}
+						whileTap={{ scale: 1 }}
+						transition={{ type: "spring", stiffness: 500 }}
+					>
 						Menu
-					</Link>
+					</MotionLink>
 				</li>
 			</ul>
 		</nav>
