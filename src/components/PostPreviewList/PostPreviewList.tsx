5fcbd03dbf491c07d4ddd0f2561066b5c8a1a623
Romain <rsire@tuta.io>
2024-12-21 17:04:27 +0900

    feat: add motionInitialDelay prop to PostPreviewList for customizable animation delays

--- src/components/PostPreviewList/PostPreviewList.tsx
+++ src/components/PostPreviewList/PostPreviewList.tsx
@@ -5,22 +5,26 @@ import style from "./PostPreviewList.module.css";
 
 type PostPreviewListProps = {
 	posts: PostSchemaLight[];
+	motionInitialDelay?: number;
 };
 
-export default function PostPreviewList({ posts }: PostPreviewListProps) {
+export default function PostPreviewList({
+	posts,
+	motionInitialDelay = 0,
+}: PostPreviewListProps) {
 	return (
 		<nav className={style.section}>
 			<ul className={style.list}>
 				{posts.map((post, index) => {
 					const motionInitialX = index % 2 === 0 ? -100 : 100;
-					const motionInitialDelay = (index + 1) * 0.1;
+					const selfDelay = (index + 1) * 0.1 + motionInitialDelay;
 					return (
 						<li className={style.postWrapper} key={post.slug}>
 							<PostPreview
 								post={post}
 								className={style.post}
 								motionInitialX={motionInitialX}
-								motionInitialDelay={motionInitialDelay}
+								motionInitialDelay={selfDelay}
 							/>
 						</li>
 					);
