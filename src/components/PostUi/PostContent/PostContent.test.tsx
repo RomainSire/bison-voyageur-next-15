d594ded6fb2b8679363588a8ba6fd7f758c4b3fd
Romain <rsire@tuta.io>
2024-12-26 16:32:31 +0900

    feat: add tests for PostContent component

---
+++ src/components/PostUi/PostContent/PostContent.test.tsx
@@ -0,0 +1,40 @@
+import { MainPictureSchema } from "@/Schemas/MainPictureSchema";
+import { PostSchema } from "@/Schemas/PostSchema";
+import { render, screen } from "@testing-library/react";
+import PostContent from "./PostContent";
+
+const mainPicture = {
+	filename_disk: "test-image.jpg",
+	width: 800,
+	height: 600,
+	title: "Test Image",
+} as MainPictureSchema;
+
+const mockPost: PostSchema = {
+	id: "1",
+	status: "published",
+	sort: 1,
+	date_created: "2024-12-01",
+	date_updated: "2024-12-01",
+	title: "Test Post",
+	slug: "test-post",
+	date: "2013-06-01",
+	tag: ["test"],
+	mainPicture: mainPicture,
+	mainPictureAlt: "Test Image",
+	summary: "This is a test post",
+	content: "This is the test post content",
+};
+
+jest.mock("../ParseMarkdown/ParseMarkdown", () => () => (
+	<div>Mocked Markdown Content</div>
+));
+
+describe("PostContent Component", () => {
+	test("renders correctly with given post", () => {
+		render(<PostContent post={mockPost} />);
+
+		expect(screen.getByText("samedi 1 juin 2013")).toBeInTheDocument();
+		expect(screen.getByText("Mocked Markdown Content")).toBeInTheDocument();
+	});
+});
