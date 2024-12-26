5a0c4f67f9a234d73e38202ed45aab70fcc40717
Romain <rsire@tuta.io>
2024-12-26 15:13:11 +0900

    feat: enhance PostHeader with motion effects and update styling for improved visuals

--- src/components/PostHeader/PostHeader.tsx
+++ src/components/PostHeader/PostHeader.tsx
@@ -1,24 +1,55 @@
+"use client";
 import directus from "@/lib/directus";
 import { PostSchema } from "@/Schemas/PostSchema";
+import { motion } from "motion/react";
 import Image from "next/image";
 import style from "./PostHeader.module.css";
 
 type PostHeaderProps = {
 	post: PostSchema;
 	className?: string;
+	motionInitialDelay?: number;
 };
 
-export default function PostHeader({ post, className }: PostHeaderProps) {
+const MotionImage = motion.create(Image);
+
+export default function PostHeader({
+	post,
+	className,
+	motionInitialDelay = 0,
+}: PostHeaderProps) {
 	return (
 		<div className={`${style.wrapper} ${className ?? ""}`}>
-			<Image
+			<MotionImage
 				src={`${directus.url}assets/${post.mainPicture.filename_disk}`}
 				alt={post.mainPictureAlt}
 				width={post.mainPicture.width ?? undefined}
 				height={post.mainPicture.height ?? undefined}
 				className={style.image}
+				initial={{ x: -15, opacity: 0 }}
+				animate={{
+					x: 0,
+					opacity: 1,
+					transition: { delay: motionInitialDelay },
+				}}
 			/>
-			<h1 className={style.title}>{post.title}</h1>
+			<motion.h1
+				className={style.title}
+				initial={{ opacity: 0, rotate: -45, y: -10 }}
+				animate={{
+					opacity: 1,
+					rotate: 0,
+					y: 0,
+					transition: {
+						delay: motionInitialDelay,
+						type: "spring",
+						stiffness: 500,
+						damping: 15,
+					},
+				}}
+			>
+				{post.title}
+			</motion.h1>
 		</div>
 	);
 }
