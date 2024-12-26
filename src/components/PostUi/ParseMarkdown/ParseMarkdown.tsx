d16d7d9550106b274277a529171f9b89ddc7d9cf
Romain <rsire@tuta.io>
2024-12-26 16:24:09 +0900

    refactor: remove styles from ParseMarkdown component

--- src/components/PostUi/ParseMarkdown/ParseMarkdown.tsx
+++ src/components/PostUi/ParseMarkdown/ParseMarkdown.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import ReactMarkdown, { type Components } from "react-markdown";
-import style from "./ParseMarkdown.module.css";
 
 type ParseContentType = {
 	markdown: string;
@@ -9,22 +8,10 @@ type ParseContentType = {
 export default function ParseMarkdown({ markdown }: ParseContentType) {
 	const components: Partial<Components> = {
 		img: ({ src, alt }) => {
-			return (
-				<Image
-					className={style.images}
-					src={src ?? ""}
-					alt={alt ?? ""}
-					width={400}
-					height={400}
-				/>
-			);
+			return <Image src={src ?? ""} alt={alt ?? ""} width={400} height={400} />;
 		},
 		p: ({ children, ...props }) => {
-			return (
-				<p className={style.paragraph} {...props}>
-					{children}
-				</p>
-			);
+			return <p {...props}>{children}</p>;
 		},
 	};
 
