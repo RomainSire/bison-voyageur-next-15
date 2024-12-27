2eb11dd08ac6e172c7d882f67282e641a6010738
Romain <rsire@tuta.io>
2024-12-27 16:28:58 +0900

    feat: enhance PostImage component with motion effects on hover and tap

--- src/components/PostUi/PostImage/PostImage.tsx
+++ src/components/PostUi/PostImage/PostImage.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import Image from "next/image";
 import style from "./PostImage.module.css";
 
@@ -6,14 +9,18 @@ type PostImageProps = {
 	alt: string;
 };
 
+const MotionImage = motion.create(Image);
+
 export default function PostImage({ src, alt }: PostImageProps) {
 	return (
-		<Image
+		<MotionImage
 			className={style.image}
 			src={src}
 			alt={alt}
 			width={500}
 			height={500}
+			whileHover={{ scale: 1.03, y: -5 }}
+			whileTap={{ scale: 0.98 }}
 		/>
 	);
 }
