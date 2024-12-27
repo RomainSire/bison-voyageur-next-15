70cb7bf7c9905fde98c76e5cc44eb8cac0662af2
Romain <rsire@tuta.io>
2024-12-27 23:49:07 +0900

    feat: add button wrapper to PostImage for opening images

--- src/components/PostUi/PostImage/PostImage.tsx
+++ src/components/PostUi/PostImage/PostImage.tsx
@@ -12,15 +12,22 @@ type PostImageProps = {
 const MotionImage = motion.create(Image);
 
 export default function PostImage({ src, alt }: PostImageProps) {
+	const onPictureClick = () => {
+		// TODO: open modal instead of new tab
+		window.open(src, "_blank");
+	};
+
 	return (
-		<MotionImage
-			className={style.image}
-			src={src}
-			alt={alt}
-			width={500}
-			height={500}
-			whileHover={{ scale: 1.03, y: -5 }}
-			whileTap={{ scale: 0.98 }}
-		/>
+		<button className={style.button} type="button" onClick={onPictureClick}>
+			<MotionImage
+				className={style.image}
+				src={src}
+				alt={alt}
+				width={500}
+				height={500}
+				whileHover={{ scale: 1.03, y: -5 }}
+				whileTap={{ scale: 0.98 }}
+			/>
+		</button>
 	);
 }
